fb80dc7e0fc477d6cf8d919a28ac6710cca8d4b2
Thaddeus <thaddeusleezx@gmail.com>
2025-02-08 00:51:27 -0500

    fe: add incorrect answer to interface

--- aqards/src/data/flashcards.ts
+++ aqards/src/data/flashcards.ts
@@ -9,6 +9,7 @@ export const flashcards: FlashcardSet = {
       image: ["image1.png"], // images extracted from the slide, links to firebase object storage
       question: "This is a question",
       slideNumber: 1, // slide that the question is from
+      incorrectAnswers: ["incorrect answer 1", "incorrect answer 2"], // incorrect answers
       additionalInfo:
         "This is info that the ai can search for outside of the slide content",
     },
@@ -18,24 +19,27 @@ export const flashcards: FlashcardSet = {
       image: ["image1.png"],
       question: "This is a question",
       slideNumber: 1,
+      incorrectAnswers: ["incorrect answer 1", "incorrect answer 2"], // incorrect answers
       additionalInfo:
         "This is info that the ai can search for outside of the slide content",
     },
     {
-      answers: ["answer 1", "answer 2"],
+      answers: ["answer 1"],
       id: "DGocNMIssojTMFjheARW",
       image: ["image1.png"],
       question: "This is a question",
       slideNumber: 2,
+      incorrectAnswers: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"], // incorrect answers
       additionalInfo:
         "This is info that the ai can search for outside of the slide content",
     },
     {
-      answers: ["answer 1", "answer 2"],
+      answers: ["answer 1"],
       id: "LeTdQrOeYbKZK0TNf3ZE",
       image: ["image1.png"],
       question: "This is a question",
       slideNumber: 2,
+      incorrectAnswers: ["incorrect answer 1", "incorrect answer 2", "incorrect answer 3"], // incorrect answers
       additionalInfo:
         "This is info that the ai can search for outside of the slide content",
     },
